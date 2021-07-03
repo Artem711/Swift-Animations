f72d9b04cfe1f1ff48ccf25aba911a76222f68e4
Artem <swift.uix@gmail.com>
2021-07-03 23:25:38 +0200

    start

---
+++ Animater/AnimaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaterApp.swift
+//  Animater
+//
+//  Created by Артём Мошнин on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animater/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animater
+//
+//  Created by Артём Мошнин on 3/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
